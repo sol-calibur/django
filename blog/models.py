a5adc551afbc66237d1b213f236793bac5f61ce4
rain caliber <66094781+s-downpour@users.noreply.github.com>
2024-09-22 15:15:37 -0500

    fix db

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    lauthor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
