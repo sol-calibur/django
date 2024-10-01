f49f327bcc4ec5430919a09a68506cd7ec65602f
rain caliber <66094781+s-downpour@users.noreply.github.com>
2024-10-01 18:03:23 -0500

    secure site

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 from .models import Post
 from .forms import PostForm
 
-# Create your views here.
+@login_required
+def post_new(request):
+    [...]
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
