d428cf1c6f46bf3a05ca16a65fe30ce9a3023d48
rain caliber <66094781+s-downpour@users.noreply.github.com>
2024-10-01 20:10:00 -0500

    fix auth

--- blog/urls.py
+++ blog/urls.py
@@ -1,16 +1,15 @@
 from django.urls import path, include
 from django.contrib import admin
-
-from django.contrib.auth import views
-
+from django.contrib.auth.views import LoginView
+from blog import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/drafts/', views.post_draft_list, name='post_draft_list'),
     path('admin/', admin.site.urls),
-    path('accounts/login/', views.LoginView.as_view(), name='login'),
-    path('', include('blog.urls')),
-
+    path('accounts/login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
